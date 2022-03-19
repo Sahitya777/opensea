01ed50d123436a34504abb7a91a2bdd3abc60fb9
Sahitya77 <sahityanijhawan@gmail.com>
2022-03-19 16:18:15 +0530

    Navbar updated!!

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,9 @@ import React from 'react'
 import opensea from '../assests/opensea.png'
 import Link from 'next/link'
 import Image from 'next/image'
+import {AiOutlineSearch} from 'react-icons/ai'
+import { CgProfile } from 'react-icons/cg'
+import { MdOutlineAccountBalanceWallet } from 'react-icons/md'
 const style = {
     wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
     logoContainer: `flex items-center cursor-pointer`,
@@ -19,8 +22,32 @@ const Header=()=>{
         <Link href="/">
             <div className={style.logoContainer}>
                 <Image src={opensea} height={40} width={40} />
+                <div className={style.logoText}>Opensea</div>
             </div>
         </Link>
+        <div className={style.searchBar}>
+            <div className={style.searchIcon}>
+                <AiOutlineSearch/>
+            </div>
+            <input className={style.searchInput} placeholder='Search items, collections, and accounts'/>
+        </div>
+        <div className={style.headerItems}>
+            <Link href='/'>
+                <div className={style.headerItem}>Collections</div>
+            </Link>
+            
+            <div className={style.headerItem}>Stats</div>
+            <div className={style.headerItem}>Resources</div>
+            <div className={style.headerItem}>Create</div> 
+            <div className={style.headerIcon}>
+                <CgProfile/>
+            </div>
+            <div className={style.headerIcon}>
+                <MdOutlineAccountBalanceWallet/>
+            </div>
+        </div>
+        
+
     </div>
 }
 
